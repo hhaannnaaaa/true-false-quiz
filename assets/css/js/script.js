fabe4393df179b532d52ae83667a8bd4237f439d
hhaannnaaaa <hannalovisaberg@gmail.com>
2023-03-17 08:47:45 +0000

    Add variables in js file

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,1 +1,6 @@
-var button = document.getElementsById("check-button");
+let answer = 0;
+let checkButton = document.getElementById('check-button');
+let xmarkButton = document.getElementById('xmark-button');
+let buttonDiv = document.querySelector('.buttons');
+let questionDiv = document.querySelector('.answer');
+let resultDiv = document.querySelector('.result');
